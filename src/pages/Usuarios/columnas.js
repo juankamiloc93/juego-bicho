7402df4bda2e0f40ca53ebb27078101be119c581
Juan Camilo Castro <juankamiloc93@gmail.com>
2026-04-12 15:28:35 -0500

    .

--- src/pages/Usuarios/columnas.js
+++ src/pages/Usuarios/columnas.js
@@ -2,18 +2,11 @@ const columnas = [
   
     {
         campo: 'name',
-<<<<<<< HEAD
-=======
         label: 'Nombre',
->>>>>>> 5cb81c9e6e23c6e7df97054f9c5c458e89c39ac4
         tipo: 'texto'
     },
     {
         campo: 'email',
-<<<<<<< HEAD
-        tipo: 'texto'
-    }, 
-=======
         label: 'Email',
         tipo: 'texto'
     }, 
@@ -22,7 +15,6 @@ const columnas = [
         label: 'Senha',
         tipo: 'texto'
     }, 
->>>>>>> 5cb81c9e6e23c6e7df97054f9c5c458e89c39ac4
 ];
 
 export default columnas;
