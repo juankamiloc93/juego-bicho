bc7a0a9dfa8bc22361908c5293b202639ce04f15
Juan Camilo Castro <juankamiloc93@gmail.com>
2026-04-25 16:50:58 -0500

    .

--- src/hooks/http/useUsuariosHttp.js
+++ src/hooks/http/useUsuariosHttp.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
+import { useToken } from "@/hooks/useToken";
 import axios from "axios";
 
 export default function useUsuariosHttp() {
@@ -9,12 +10,19 @@ export default function useUsuariosHttp() {
   const apiBaseUrl = 'http://localhost:8000/api';
   const url = 'users'
 
-
+  const getHeaders = async () => {
+    const { getToken } = useToken();
+    const token = await getToken();
+    const headers = {Authorization: `Bearer ${token}`}
+    return headers
+  }
+  
   const fetchUsuarios = useCallback(async () => {    
+    const headers = await getHeaders();
     setCargando(true);
     setError(null);
     try {
-      const { data } = await axios.get(`${apiBaseUrl}/${url}`);      
+      const { data } = await axios.get(`${apiBaseUrl}/${url}`, {headers});      
       setUsuarios(data);   
       localStorage.setItem("usuarios", JSON.stringify(data));
     } catch (err) {
@@ -25,10 +33,11 @@ export default function useUsuariosHttp() {
   }, []);
 
   const crearUsuario = async (nuevoUsuario) => {
+     const headers = await getHeaders();
     setCargando(true);
     setError(null);
     try {
-      await axios.post(`${apiBaseUrl}/${url}`, nuevoUsuario);
+      await axios.post(`${apiBaseUrl}/${url}`, nuevoUsuario, {headers});
       await fetchUsuarios(); // refrescar lista
     } catch (err) {
       setError(err.response?.data?.message || err.message);
@@ -39,10 +48,11 @@ export default function useUsuariosHttp() {
 
 
   const actualizarUsuario = async (id, datosActualizados) => {
+     const headers = await getHeaders();
     setCargando(true);
     setError(null);
     try {
-      await axios.put(`${apiBaseUrl}/${url}/${id}`, datosActualizados);
+      await axios.put(`${apiBaseUrl}/${url}/${id}`, datosActualizados, {headers});
       await fetchUsuarios();
     } catch (err) {
       setError(err.response?.data?.message || err.message);
@@ -53,10 +63,11 @@ export default function useUsuariosHttp() {
 
 
   const eliminarUsuario = async (id) => {
+    const headers = await getHeaders();
     setCargando(true);
     setError(null);
     try {
-      await axios.delete(`${apiBaseUrl}/${url}/${id}`);
+      await axios.delete(`${apiBaseUrl}/${url}/${id}`, {headers});
       await fetchUsuarios();
     } catch (err) {
       setError(err.response?.data?.message || err.message);
